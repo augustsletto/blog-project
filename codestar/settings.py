efa2db6ea5d3b29217025280b792333400472566
august sletto <augustsletto@gmail.com>
2024-02-15 13:02:16 +0000

    models and settings fix

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-augustslett-blogproject-qw9a3k15sya.ws-eu108.gitpod.io', '.herokuapp.com']
 
